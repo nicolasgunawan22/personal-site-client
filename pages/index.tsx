f9dee4ac56228c77ea749c217dcd00cbfe708e9c
nicolasgunawan22 <nicolasgunawan0@gmail.com>
2022-05-23 21:38:50 +0700

    fix: blog section on small view

--- pages/index.tsx
+++ pages/index.tsx
@@ -71,7 +71,7 @@ const Home: NextPage = () => {
             <h3>latest article</h3>
             <button className="font-bold">see all</button>
           </div>
-          <div className="grid grid-cols-3 w-3/4 mx-auto gap-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-3/4 mx-auto gap-4">
             {[1, 2, 3].map((i, index) => (
               <div key={index} className="relative aspect-square group">
                 <div className="absolute aspect-square p-4 flex flex-col justify-end gap-2 z-10 text-slate-50">
