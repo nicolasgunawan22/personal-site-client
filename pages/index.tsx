c5049f9feb054b77b50c2cbf2947b295b8bbc2f2
nicolasgunawan22 <nicolasgunawan0@gmail.com>
2022-05-23 21:30:00 +0700

    add blog and contact me section

--- pages/index.tsx
+++ pages/index.tsx
@@ -67,13 +67,49 @@ const Home: NextPage = () => {
           </div>
         </section>
         <section id="#blog" className="min-h-screen flex flex-col justify-center items-start gap-8">
-          <div className="flex justify-between w-1/2 mx-auto border-b-2 border-b-[#202020] pb-2">
+          <div className="flex justify-between w-3/4 mx-auto border-b-2 border-b-[#202020] pb-2">
             <h3>latest article</h3>
             <button className="font-bold">see all</button>
           </div>
-          <div>
-
+          <div className="grid grid-cols-3 w-3/4 mx-auto gap-4">
+            {[1, 2, 3].map((i, index) => (
+              <div key={index} className="relative aspect-square group">
+                <div className="absolute aspect-square p-4 flex flex-col justify-end gap-2 z-10 text-slate-50">
+                  <div className='text-xs text-left flex justify-between items-center translate-y-16 group-hover:translate-y-0 transition-all ease-in-out duration-500'>
+                    <span className="bg-slate-900 text-white font-bold px-1 py-0.5">Coding</span>
+                    <span className="font-bold">2020-01-01</span>
+                  </div>
+                  <h5 className="text-md text-left font-bold translate-y-16 group-hover:translate-y-0 transition-all ease-in-out duration-500">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Et sint eveniet consectetur.
+                  </h5>
+                  <p className='text-xs font-light translate-y-16 opacity-0 group-hover:translate-y-0 group-hover:opacity-100 transition-all ease-in-out duration-500'>
+                    Nam vero explicabo blanditiis commodi earum quae mollitia! Fuga, ad cumque dicta ab distinctio facilis repellat mollitia?
+                  </p>
+                </div>
+                <div className="z-0">
+                  <Image
+                    src="https://images.unsplash.com/photo-1475823678248-624fc6f85785?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870"
+                    alt="me"
+                    className='brightness-75'
+                    layout="fill"
+                    objectFit="cover"
+                  />
+                </div>
+              </div>
+            ))}
           </div>
+        </section>
+        <section id="#blog" className="min-h-screen flex flex-col justify-center items-center gap-12">
+          <h2 className='text-2xl font-bold'>contact me</h2>
+          <form className='flex flex-col gap-4 w-2/3' onSubmit={() => { }}>
+            <div className='flex gap-4'>
+              <input placeholder="Your Name" className="bg-transparent border-2 border-[#202020] p-2 w-full" type="text" />
+              <input placeholder="Your Email" className="bg-transparent border-2 border-[#202020] p-2 w-full" type="text" />
+            </div>
+            <input placeholder="Subject" className="bg-transparent border-2 border-[#202020] p-2 w-full" type="text" />
+            <textarea placeholder="Message" className="bg-transparent border-2 border-[#202020] p-2 w-full" rows={8}></textarea>
+            <button className='text-lg px-6 py-3 bg-[#D9D9D9] mx-auto drop-shadow-lg hover:bg-[#202020] hover:text-white transition-all ease-in-out' type="submit">Send Message</button>
+          </form>
         </section>
       </Container>
 
